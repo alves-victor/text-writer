83d7ee2936304aadfe383475a6f3439724f5dc55
Victor Alves <victorhugo_sp4@hotmail.com>
2023-08-29 18:43:33 -0300

    estruturação da tela de login

--- src/components/signin/index.tsx
+++ src/components/signin/index.tsx
@@ -1,5 +1,16 @@
+import './style.css';
+
 export default function Signin(){
     return(
-        <div>opa</div>
+        <main className='main'>
+            <p className='text'>Entre com sua conta!</p>
+            <div className='inputSection'>
+                <label htmlFor="text">Email</label>
+                <input type="text" name="email" id="email" />
+                <label htmlFor="password">Senha</label>
+                <input type="password" name="password" id="password" />
+            </div>
+            <button className='btn'>Login</button>
+        </main>
     )
 }
