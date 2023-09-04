f4e1a52f94d1ce73dcb6768cb7926d5319455190
Victor Alves <victorhugo_sp4@hotmail.com>
2023-09-04 19:21:51 -0300

    adição de top section para mais opções

--- src/components/text-area/index.tsx
+++ src/components/text-area/index.tsx
@@ -3,7 +3,10 @@ import './style.css'
 export default function Text(){
     return(
         <main className="main">
-            <textarea name="content" className="text-area" cols={30} rows={10}></textarea>
+            <section className='top-section'>
+                <button className='btn'>salvar</button>
+            </section>
+            <textarea name="content" className="text-area" cols={30} rows={25}></textarea>
         </main>
     )
 }
