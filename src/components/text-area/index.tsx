a473ee34af7df55818f5618c4eda16bad1f15a9e
Victor Alves <victorhugo_sp4@hotmail.com>
2023-09-09 21:30:50 -0300

    controle de edição e salvamento dos arquivos de texto

--- src/components/text-area/index.tsx
+++ src/components/text-area/index.tsx
@@ -1,12 +1,71 @@
+'use client'
+import { useState } from 'react'
 import './style.css'
 
+declare global{
+    interface Window{
+        showOpenFilePicker(argument: any): any;
+        showSaveFilePicker(arument: any): any;
+    }
+}
+
 export default function Text(){
+    const [text, saveText] = useState("");
+    let fileHandle: any;
+
+    function handleChange(event: any){
+        saveText(event.target.value);
+    }
+
+    let options = {
+        types: [{
+            description: 'Apenas arquivos txt',
+            accept: {
+                'text/*': ['.txt']
+            }
+        }],
+        excludeAcceptAllOption: true,
+        multiple: false
+    }
+
+    async function chooseFile(){
+        try{
+            [fileHandle] = await window.showOpenFilePicker(options);
+        }catch{ return }
+        
+        let fileData = await fileHandle.getFile();
+        Object.assign(options, {suggestedName:fileData['name']} )
+        let textareaContent = await fileData.text();
+        saveText(textareaContent);
+    }
+
+    async function handleSave(){
+        let stream = await fileHandle.createWritable();
+        await stream.write(text);
+        await stream.close();
+        alert("Arquivo Salvo!");
+        fileHandle = "";
+        saveText("");
+    }
+
+    async function saveFile(){
+        try{
+            fileHandle = await window.showSaveFilePicker(options);
+            handleSave();
+        }catch{ return }
+
+
+    }   
+
     return(
         <main className="main">
             <section className='top-section'>
-                <button className='btn'>salvar</button>
+                <button className='btn' onClick={chooseFile}>Editar arquivo existente</button>
             </section>
-            <textarea name="content" className="text-area" cols={30} rows={25}></textarea>
+            <textarea onChange={handleChange} value={text} cols={30} rows={22} className="text-area" ></textarea>
+            <section className='bottom-section'>
+                <button className='btn' onClick={saveFile}>Salvar</button>
+            </section>
         </main>
     )
 }
